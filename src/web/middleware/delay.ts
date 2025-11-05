75b5d240839a9486b8168ba3fba58ea1a73879b1
RodrigoDornelles <rodrigo.dornelles.poa@gmail.com>
2025-11-05 17:57:01 -0300

    feat: add delay for first request

--- src/web/middleware/delay.ts
+++ src/web/middleware/delay.ts
@@ -7,12 +7,27 @@ const getRandomInt = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+let isFirstRequest = true;
+
 export const delayMiddleware: Middleware = async (req, next) => {
   const delayVar = process.env.DELAY;
 
   if (delayVar) {
     const parts = delayVar.split(':');
-    if (parts.length === 2) {
+    if (parts.length === 3) {
+      const first = parseInt(parts[0], 10);
+      const min = parseInt(parts[1], 10);
+      const max = parseInt(parts[2], 10);
+      if (!isNaN(first) && !isNaN(min) && !isNaN(max)) {
+        if (isFirstRequest) {
+          await delay(first);
+          isFirstRequest = false;
+        } else {
+          const randomDelay = getRandomInt(min, max);
+          await delay(randomDelay);
+        }
+      }
+    } else if (parts.length === 2) {
       const min = parseInt(parts[0], 10);
       const max = parseInt(parts[1], 10);
       if (!isNaN(min) && !isNaN(max)) {
